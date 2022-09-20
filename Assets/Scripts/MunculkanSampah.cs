fb2947d7c3393b57feb517234b7501d47f1fb2a9
gesekanmagnet <113659944+gesekanmagnet@users.noreply.github.com>
2022-09-20 11:20:34 +0700

    Add files via upload

---
+++ Assets/Scripts/MunculkanSampah.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MunculkanSampah : MonoBehaviour {
+
+	public float jeda = 0.8f;
+	private float timer;
+	public GameObject[] sampah;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		if(timer > jeda)
+		{
+			int random = Random.Range(0, sampah.Length);
+			Instantiate(sampah[random], transform.position, transform.rotation);
+			timer = 0;
+		}
+	}
+}
---
+++ Assets/Scripts/deteksi.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class deteksi : MonoBehaviour {
+
+	public string nameTag;
+	public AudioClip benar;
+	public AudioClip salah;
+		private AudioSource mediaPlayerBenar;
+		private AudioSource mediaPlayerSalah;
+		public Text textScore;
+
+	// Use this for initialization
+	void Start () {
+		mediaPlayerBenar = gameObject.AddComponent<AudioSource>();
+		mediaPlayerBenar.clip = benar;
+		mediaPlayerSalah = gameObject.AddComponent<AudioSource>();
+		mediaPlayerSalah.clip = salah;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.tag.Equals(nameTag))
+		{
+			Data.score += 25;
+			textScore.text = Data.score.ToString();
+			Destroy(other.gameObject);
+			mediaPlayerBenar.Play();
+		}
+		else
+		{
+			Data.score -= 5;
+			textScore.text = Data.score.ToString();
+			Destroy(other.gameObject);
+			mediaPlayerSalah.Play();
+		}
+	}
+}
